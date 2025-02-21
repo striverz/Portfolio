821641599a0bc033f1153dae65bce1b1ac83049d
striverz <striverzarc@gmail.com>
2025-02-21 18:57:18 +0530

    added dynamic navbar functionlay for moving

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/MovingBorders";
 
 const Experience = () => {
   return (
-    <div className="py-20 w-full">
+    <div className="py-20 w-full" id="expertise">
       <h1 className="heading">
         My Experience & <span className="text-purple">Expertise</span>
       </h1>
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -8,7 +8,7 @@ import { it } from "node:test";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
